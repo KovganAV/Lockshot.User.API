aac668337ae19088ccbcdd2e4e5dd67fb1b82e35
Marsisanya <kovgana25@gmail.com>
2025-01-20 14:46:23 +0300

    db

---
+++ Lockshot.User.API/Lockshot.User.API/Migrations/20250115162235_test3.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Lockshot.User.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class test3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Guns");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Users",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Hits",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Hits",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Hits",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Hits",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.CreateTable(
+                name: "Guns",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Bulets = table.Column<int>(type: "integer", nullable: false),
+                    Calibre = table.Column<string>(type: "text", nullable: false),
+                    FiringMode = table.Column<string>(type: "text", nullable: false),
+                    MaxDistance = table.Column<double>(type: "double precision", nullable: false),
+                    NameGun = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    WeaponType = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guns", x => x.Id);
+                });
+        }
+    }
+}
---
+++ Lockshot.User.API/Lockshot.User.API/Migrations/20250120084353_AddPhotoUrlToUser.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lockshot.User.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPhotoUrlToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Удаляем изменение типа для Id, если оно есть
+            // migrationBuilder.AlterColumn<Guid>(
+            //     name: "Id",
+            //     table: "Users",
+            //     type: "uuid",
+            //     nullable: false);
+
+            // Добавляем только новый столбец
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Удаляем только новый столбец
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                table: "Users");
+        }
+
+    }
+}
